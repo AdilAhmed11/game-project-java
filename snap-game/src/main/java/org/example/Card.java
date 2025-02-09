0154947f82b688314b375d3cefcb835e9e67345c
Adil Ahmed <adilahmed749@gmail.com>
2025-02-09 18:16:04 +0000

    Populating the CardGame class. Added the fields, constructor, and started the populateDeck method

--- snap-game/src/main/java/org/example/Card.java
+++ snap-game/src/main/java/org/example/Card.java
@@ -1,22 +1,22 @@
 package org.example;
 
 public class Card {
-    private String suit; // Field for heart, diamond, club, spade
-    private String symbol; // Field for 2,3,4,5,6,7,8,9,10,J,Q,K,A
-    private int value; // Field for 2,3,4,5,6,7,8,9,10,11,12,13,14
+    private String suit; // Field to store the card's suit (i.e. heart, diamond, club, spade)
+    private String symbol; // Field store the symbol (i.e. 2,3,4,5,6,7,8,9,10,J,Q,K,A)
+    private int value; // Field to store the value (i.e. 2,3,4,5,6,7,8,9,10,11,12,13,14)
 
-    // Constructor
+    // Constructor - A special method to initialise objects.
     public Card(String suit, String symbol, int value) {
+        // Initialises the card with a suit, symbol, and value
         this.suit = suit;
         this.symbol = symbol;
         this.value = value;
     }
 
     // toString method to describe the class
+    // Returns a string representation of a card
     public String toString() {
         return symbol + suit; // E.g. 4 Hearts, J Clubs, etc
     }
-
-
 
 }
--- snap-game/src/main/java/org/example/CardGame.java
+++ snap-game/src/main/java/org/example/CardGame.java
@@ -3,6 +3,22 @@ package org.example;
 import java.util.ArrayList;
 
 public class CardGame {
-    
+    // Create the fields for the CardGame class
+    private ArrayList<Card> deckOfCards; // Field to store the deck of cards
 
+    // Constructor - A special method to initialise objects.
+    // Initialising deckOfCards array list AND the populateDeck method below
+    public CardGame(String name) {
+        this.deckOfCards = new ArrayList<>();
+        populateDeck(); // Populate the deck when the game is created
+    }
+
+    // Method to populate the deck
+    private void populateDeck() {
+        String[] suits = {"♥", "♦", "♣", "♠"}; // Unicode for suits
+        String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+
+    }
 }
